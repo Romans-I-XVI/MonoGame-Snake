096a68a3fed8938baeca6fbc0536957ab5d8aaf2
Austin Sojka <austinsojka@gmail.com>
2020-04-02 15:42:40 -0500

    Added RoomPlay file

--- SnakeShared/SnakeShared/Rooms/RoomMain.cs
+++ SnakeShared/SnakeShared/Rooms/RoomMain.cs
@@ -6,7 +6,9 @@ namespace Snake.Rooms
 {
 	public class RoomMain : Room
 	{
-		public override void onSwitchTo(Room previous_room, Dictionary<string, object> args) {}
+		public override void onSwitchTo(Room previous_room, Dictionary<string, object> args) {
+			Engine.ChangeRoom<RoomPlay>();
+		}
 
 		public override void onSwitchAway(Room next_room) {}
 	}
---
+++ SnakeShared/SnakeShared/Rooms/RoomPlay.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using MonoEngine;
+
+namespace Snake.Rooms
+{
+	public class RoomPlay : Room
+	{
+		public override void onSwitchTo(Room previous_room, Dictionary<string, object> args) {
+		}
+
+		public override void onSwitchAway(Room next_room) {
+		}
+	}
+}
