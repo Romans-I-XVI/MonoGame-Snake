88c7b96a4a02137518e8026616decb77e647b082
asojka09 <austinsojka@gmail.com>
2020-04-04 18:51:47 -0500

    Removed ControlReset

--- SnakeShared/SnakeShared/Rooms/RoomPlay.cs
+++ SnakeShared/SnakeShared/Rooms/RoomPlay.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using MonoEngine;
 using Snake.Entities;
@@ -23,30 +22,9 @@ namespace Snake.Rooms
 			Engine.SpawnInstance<ControlPause>();
 			Engine.SpawnInstance<ControlFoodSpawner>();
 			Engine.SpawnInstance<Background>();
-
-			Engine.SpawnInstance(new ControlReset());
 		}
 
 		public override void onSwitchAway(Room next_room) {
-		}
-
-		private class ControlReset : Entity
-		{
-			public override void onKeyDown(KeyboardEventArgs e) {
-				base.onKeyDown(e);
-
-				if (e.Key == Keys.R || e.Key == Keys.Space) {
-					Engine.ResetRoom();
-				}
-			}
-
-			public override void onButtonDown(GamePadEventArgs e) {
-				base.onButtonDown(e);
-
-				if (e.Button == Buttons.Y) {
-					Engine.ResetRoom();
-				}
-			}
 		}
 	}
 }
