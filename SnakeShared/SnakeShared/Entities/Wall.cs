a66f3cbc89a0a62890746e22852b4a0877e43562
Austin Sojka <austinsojka@gmail.com>
2020-04-15 15:20:14 -0500

    Made changes to the Wall entity

--- SnakeShared/SnakeShared/Entities/Wall.cs
+++ SnakeShared/SnakeShared/Entities/Wall.cs
@@ -5,14 +5,16 @@ namespace Snake.Entities
 {
 	public class Wall : Entity
 	{
-		public Wall(int x, int y) {
+		public Wall(int x, int y, float scale = 1) {
 			this.Depth = 1;
 			this.Position = new Vector2(x, y);
 			var texture = ContentHolder.Get(Settings.CurrentWall);
-			var region = new Region(texture, 0, 0, texture.Width, texture.Height, texture.Width / 2, texture.Height / 2);
-			var sprite = new Sprite(region);
+			var region = new Region(texture, 0, 0, texture.Width, texture.Height, 0, 0);
+			var sprite = new Sprite(region) {
+				Scale = new Vector2(scale)
+			};
 			this.AddSprite("main", sprite);
-			this.AddColliderRectangle("main", -texture.Width / 2, -texture.Height / 2, texture.Width, texture.Height);
+			this.AddColliderRectangle("main", 0, 0, (int)(texture.Width * scale), (int)(texture.Height * scale));
 		}
 	}
 }
