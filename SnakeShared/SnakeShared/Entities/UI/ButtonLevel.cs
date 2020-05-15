a3e78d024cd120ef46e554d1dbf87fb2e848ece6
asojka09 <austinsojka@gmail.com>
2020-05-15 08:37:15 -0500

    Added lock draw locations

--- SnakeShared/SnakeShared/Entities/UI/ButtonLevel.cs
+++ SnakeShared/SnakeShared/Entities/UI/ButtonLevel.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using Snake.Enums;
 
 namespace Snake.Entities.UI
@@ -17,14 +18,15 @@ namespace Snake.Entities.UI
 			get {
 				if (this._drawData == null) {
 					if (this.IsUnlocked) {
-						// Do something to set up draw data
+						this._drawData = new DrawLocations[0];
 					} else {
-						// Do something to set up lock data
+						this._drawData = new [] {
+							new DrawLocations(DrawDataTextures.Snake, 0.3125f, this.GetLockDrawLocations()),
+						};
 					}
 				}
 
-				// return this._drawData;
-				return new DrawLocations[0];
+				return this._drawData;
 			}
 		}
 
@@ -51,6 +53,84 @@ namespace Snake.Entities.UI
 			}
 
 			return (highest_score >= required_score);
+		}
+
+		private Vector2[] GetLockDrawLocations() {
+			return new[] {
+				new Vector2(58, 64),
+				new Vector2(63, 64),
+				new Vector2(68, 64),
+				new Vector2(78, 64),
+				new Vector2(83, 64),
+				new Vector2(88, 64),
+				new Vector2(53, 59),
+				new Vector2(73, 39),
+				new Vector2(78, 44),
+				new Vector2(68, 44),
+				new Vector2(68, 44),
+				new Vector2(58, 59),
+				new Vector2(63, 59),
+				new Vector2(68, 59),
+				new Vector2(78, 59),
+				new Vector2(83, 59),
+				new Vector2(88, 59),
+				new Vector2(93, 59),
+				new Vector2(53, 54),
+				new Vector2(58, 54),
+				new Vector2(63, 54),
+				new Vector2(68, 54),
+				new Vector2(78, 54),
+				new Vector2(83, 54),
+				new Vector2(88, 54),
+				new Vector2(93, 54),
+				new Vector2(53, 49),
+				new Vector2(58, 49),
+				new Vector2(63, 49),
+				new Vector2(68, 49),
+				new Vector2(78, 49),
+				new Vector2(83, 49),
+				new Vector2(88, 49),
+				new Vector2(93, 49),
+				new Vector2(53, 44),
+				new Vector2(58, 44),
+				new Vector2(63, 44),
+				new Vector2(83, 44),
+				new Vector2(88, 44),
+				new Vector2(93, 44),
+				new Vector2(73, 64),
+				new Vector2(73, 59),
+				new Vector2(53, 39),
+				new Vector2(58, 39),
+				new Vector2(63, 39),
+				new Vector2(68, 39),
+				new Vector2(78, 39),
+				new Vector2(83, 39),
+				new Vector2(88, 39),
+				new Vector2(93, 39),
+				new Vector2(58, 34),
+				new Vector2(63, 34),
+				new Vector2(68, 34),
+				new Vector2(73, 34),
+				new Vector2(78, 34),
+				new Vector2(83, 34),
+				new Vector2(88, 34),
+				new Vector2(88, 29),
+				new Vector2(83, 29),
+				new Vector2(63, 29),
+				new Vector2(58, 29),
+				new Vector2(58, 24),
+				new Vector2(63, 24),
+				new Vector2(88, 24),
+				new Vector2(83, 24),
+				new Vector2(83, 19),
+				new Vector2(78, 19),
+				new Vector2(73, 19),
+				new Vector2(68, 19),
+				new Vector2(78, 14),
+				new Vector2(73, 14),
+				new Vector2(68, 14),
+				new Vector2(63, 19),
+			};
 		}
 	}
 }
